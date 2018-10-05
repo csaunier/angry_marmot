4e053fa756b3c10d153f71d69d0b6f154dee7b9c
Cédric Saunier <cedric.saunier@fullsix.com>
2018-10-05 14:08:37 +0200

    add icon

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ window.onload = () => {
     const play = document.getElementById('play');
     const video = document.getElementById('video');
 
+    video.currentTime = 0;
+
     play.addEventListener('click', () => {
         video.currentTime = 0;
         video.play();
